2410274cd2a7d14d8ebfb47b46ec91159d964688
VSAlmeida <vinicius.sebadelhe@gmail.com>
2020-12-10 14:17:26 -0300

    Added Enemy AI

---
+++ Game-TCC/Assets/Scripts/Game/Enemy/EnemyFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public NavMeshAgent enemy;
+    public Transform Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(Player.position, enemy.transform.position) < 50)
+        {
+            enemy.SetDestination(Player.position);
+        } else
+        {
+            enemy.SetDestination(enemy.transform.position);
+        }
+        
+    }
+}
