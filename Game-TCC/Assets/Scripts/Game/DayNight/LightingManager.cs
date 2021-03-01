21ef84b7900e826777450b8eb5ceb94122571b32
VSAlmeida <vinicius.sebadelhe@gmail.com>
2021-03-01 00:54:18 -0300

    Add Day Night Cycle

---
+++ Game-TCC/Assets/Scripts/Game/DayNight/LightingManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class LightingManager : MonoBehaviour
+{
+
+    [SerializeField] private Light DirectionalLight;
+    [SerializeField] private LightingPreset Preset;
+    [SerializeField, Range(0, 600)] private float TimeOfDay;
+
+    private void Update()
+    {
+        if (Preset == null)
+            return;
+
+        if (Application.isPlaying)
+        {
+            TimeOfDay += Time.deltaTime;
+            TimeOfDay %= 600;
+            UpdateLighting(TimeOfDay / 600f);
+        }
+        else
+        {
+            UpdateLighting(TimeOfDay / 600f);
+        }
+    }
+
+
+    private void UpdateLighting(float timePercent)
+    {
+        RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
+        RenderSettings.fogColor = Preset.FogColor.Evaluate(timePercent);
+
+       
+        if (DirectionalLight != null)
+        {
+            DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
+        }
+
+    }
+
+    private void OnValidate()
+    {
+        if (DirectionalLight != null)
+            return;
+      
+        if (RenderSettings.sun != null)
+        {
+            DirectionalLight = RenderSettings.sun;
+        }
+        
+        else
+        {
+            Light[] lights = GameObject.FindObjectsOfType<Light>();
+            foreach (Light light in lights)
+            {
+                if (light.type == LightType.Directional)
+                {
+                    DirectionalLight = light;
+                    return;
+                }
+            }
+        }
+    }
+}
